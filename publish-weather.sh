bc506f08e4b74ec12d3a41014c8a28b544923b3e
André Wendt <awendt@freeshell.org>
2017-02-19 21:04:13 +0100

    Change region to Frankfurt

--- publish-weather.sh
+++ publish-weather.sh
@@ -4,5 +4,5 @@ if [ $# -eq 0 ]; then
   exit 1
 fi
 sudo ./dht_metric.py 11 4 $1 > /tmp/$$.json
-aws cloudwatch put-metric-data --region eu-west-1 --namespace "Home" --metric-data file:///tmp/$$.json
+aws cloudwatch put-metric-data --region eu-central-1 --namespace "Home" --metric-data file:///tmp/$$.json
 rm -f /tmp/$$.json

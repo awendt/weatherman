32604450c50c02d6f0d9569a697bf57bcfd3f507
André Wendt <awendt@freeshell.org>
2017-04-08 21:18:14 +0200

    Update to DHT22

--- publish-weather.sh
+++ publish-weather.sh
@@ -3,6 +3,6 @@
 if [ $# -eq 0 ]; then
   exit 1
 fi
-sudo ./dht_metric.py 11 4 $1 > /tmp/$$.json
+sudo ./dht_metric.py 22 4 $1 > /tmp/$$.json
 aws cloudwatch put-metric-data --region eu-central-1 --namespace "Home" --metric-data file:///tmp/$$.json
 rm -f /tmp/$$.json
